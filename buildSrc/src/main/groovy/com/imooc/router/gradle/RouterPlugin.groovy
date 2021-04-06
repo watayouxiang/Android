25692b98473930fd6304081a09a0a5486778442f
watayouxiang <watayouxiang@qq.com>
2021-04-06 10:16:36 +0800

    empty

--- buildSrc/src/main/groovy/com/imooc/router/gradle/RouterPlugin.groovy
+++ buildSrc/src/main/groovy/com/imooc/router/gradle/RouterPlugin.groovy
@@ -7,6 +7,23 @@ class RouterPlugin implements Plugin<Project> {
     // 实现apply方法，注入插件的逻辑
     @Override
     void apply(Project project) {
+
+        // 1、自动帮助用户传递路径参数到注解处理器中
+        // 2、实现旧的构建产物的自动清理
+        // 3、在 javac 任务后，汇总生成文档
+        //
+        //     kapt {
+        //        arguments {
+        //            arg("root_project_dir", rootProject.projectDir.absolutePath)
+        //        }
+        //    }
+
+        if (project.extensions.findByName("kapt") != null) {
+            project.extensions.findByName("kapt").arguments {
+                arg("root_project_dir", project.rootProject.projectDir.absolutePath)
+            }
+        }
+
         println("i am from RouterPlugin, apply from ${project.name}")
 
         // 创建 Extension
