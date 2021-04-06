4a650068a11351a3d459ecbd8f1b2237214e5894
watayouxiang <watayouxiang@qq.com>
2021-04-06 10:57:52 +0800

    empty

--- buildSrc/src/main/groovy/com/imooc/router/gradle/RouterPlugin.groovy
+++ buildSrc/src/main/groovy/com/imooc/router/gradle/RouterPlugin.groovy
@@ -1,5 +1,6 @@
 package com.imooc.router.gradle
 
+import groovy.json.JsonSlurper
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -9,18 +10,23 @@ class RouterPlugin implements Plugin<Project> {
     void apply(Project project) {
 
         // 1、自动帮助用户传递路径参数到注解处理器中
-        // 2、实现旧的构建产物的自动清理
-        // 3、在 javac 任务后，汇总生成文档
-        //
         //     kapt {
         //        arguments {
         //            arg("root_project_dir", rootProject.projectDir.absolutePath)
         //        }
         //    }
-
         if (project.extensions.findByName("kapt") != null) {
             project.extensions.findByName("kapt").arguments {
                 arg("root_project_dir", project.rootProject.projectDir.absolutePath)
+            }
+        }
+
+        // 2、实现旧的构建产物的自动清理
+        project.clean.doFirst {
+            // 删除上一次构建生成的 router_mapping 目录
+            File routerMappingDir = new File(project.rootProject.projectDir, "router_mapping")
+            if (routerMappingDir.exists()) {
+                routerMappingDir.deleteDir()
             }
         }
 
@@ -33,6 +39,56 @@ class RouterPlugin implements Plugin<Project> {
         project.afterEvaluate {
             RouterExtension extension = project["router"]
             println("用户设置的 wikiDir 路径：${extension.wikiDir}")
+
+            // 3、在 javac 任务 (compileDebugJavaWithJavac) 后，汇总生成文档
+            project.tasks.findAll { task ->
+                task.name.startsWith('compile') && task.name.endsWith('JavaWithJavac')
+            } each { task ->
+                task.doLast {
+                    File routerMappingDir = new File(project.rootProject.projectDir, "router_mapping")
+                    if (!routerMappingDir.exists()) {
+                        return
+                    }
+                    File[] allChildFiles = routerMappingDir.listFiles()
+                    if (allChildFiles.length < 1) {
+                        return
+                    }
+
+                    StringBuilder markdownBuilder = new StringBuilder()
+                    markdownBuilder.append("# 页面文档\n\n")
+                    allChildFiles.each { child ->
+                        if (child.name.endsWith(".json")) {
+                            JsonSlurper jsonSlurper = new JsonSlurper()
+                            def content = jsonSlurper.parse(child)
+                            content.each { innerContent ->
+                                def url = innerContent['url']
+                                def description = innerContent['description']
+                                def realPath = innerContent['realPath']
+                                markdownBuilder.append("## $description\n")
+                                markdownBuilder.append("- url: $url\n")
+                                markdownBuilder.append("- realPath: $realPath\n\n")
+                            }
+                        }
+                    }
+
+                    File wikiFileDir = new File(extension.wikiDir)
+                    if (!wikiFileDir.exists()) {
+                        wikiFileDir.mkdir()
+                    }
+                    File wikiFile = new File(wikiFileDir, "页面文档.md")
+                    if (wikiFile.exists()) {
+                        wikiFile.delete()
+                    }
+                    wikiFile.write(markdownBuilder.toString())
+                }
+            }
+            // 测试 MD文档 是否生成
+            // $ ./gradlew clean -q
+            // $ ./gradlew :androiddemo:assembleDebug -q
+
+
+
+
         }
     }
 }
